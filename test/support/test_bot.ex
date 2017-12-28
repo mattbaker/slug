263b7e961192f775a74062012d815b4ddba0bd00
Matt Baker <mbaker.pdx@gmail.com>
2017-12-28 12:09:26 -0800

    drop commented code

--- test/support/test_bot.ex
+++ test/support/test_bot.ex
@@ -5,12 +5,4 @@ defmodule DasBot.Test.Support.TestBot do
     send(self(), :example_function_slug)
     event |> Event.add_metadata(:example_function_slug, true)
   end
-
-  # def send_to_channel(channel, text) do
-  #   send(self(), {:send_to_channel, channel, text})
-  # end
-
-  # def send_text(channel_id, text) do
-  #   send(self(), {:send_text, channel_id, text})
-  # end
 end
